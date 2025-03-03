762c1c22e80f3c3b1284629362187654f163a256
Brandon Keepers <brandon@opensoul.org>
2025-03-03 12:46:18 -0500

    verify NativeWind installation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,3 @@
-import '~/global.css';
-
 import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import { registerRootComponent } from 'expo';
-import { ExpoRoot } from 'expo-router';
+import "@expo/metro-runtime";
+import { registerRootComponent } from "expo";
+import { ExpoRoot } from "expo-router";
+import { verifyInstallation } from "nativewind";
+import "~/global.css";
 
 // https://docs.expo.dev/router/reference/troubleshooting/#expo_router_app_root-not-defined
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
-  const ctx = require.context('./app');
+  verifyInstallation();
+  const ctx = require.context("./app");
   return <ExpoRoot context={ctx} />;
 }
 
