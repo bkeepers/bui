460cbca145271428f947fa47d31d37e5fd9ce9fd
Brandon Keepers <brandon@opensoul.org>
2025-03-04 16:15:06 -0500

    Use @signalk/client

--- hooks/useSignalK.ts
+++ hooks/useSignalK.ts
@@ -1,20 +1,46 @@
 import { useEffect, useState } from "react";
+import Client from '@signalk/client'
+import ObjectPath from 'object-path';
 import type { Vessel } from '~/types/signalk';
+import type { SignalKDeltaMessageSchema } from "~/types/signalk-delta";
+
+const SIGNALK_SETTINGS = {
+  hostname: "pi.local",
+  port: 3000,
+  useTLS: false,
+  reconnect: true,
+  autoConnect: true,
+  sendMeta: "all",
+  deltaStreamBehaviour: 'self',
+  sendCachedValues: true,
+};
+
 
 export function useSignalK() {
   const [data, setData] = useState<Vessel>({});
 
+  function processDelta(delta: SignalKDeltaMessageSchema) {
+    setData(previousData => {
+      delta.updates.forEach(update => {
+        "meta" in update && update.meta.forEach(({ path, value }) => {
+          ObjectPath.set(previousData, `${path}.meta`, value);
+        });
+
+        "values" in update && update.values.forEach(({ path, value }) => {
+          const before = ObjectPath.get(previousData, path);
+          ObjectPath.set(previousData, path, { ...before, value });
+        });
+      });
+
+      return { ...previousData };
+    });
+  }
+
   useEffect(() => {
-    async function doFetch() {
-      const res = await fetch("http://pi.local:3000/signalk/v1/api/vessels/self");
-      if (!res.ok) throw new Error(res.statusText);
-
-      const self = await res.json();
-      setData(self);
-    }
-
-    doFetch();
-  }, [])
+    const client = new Client(SIGNALK_SETTINGS)
+    client.on('delta', processDelta);
+    return () => { client.disconnect() }
+  }, []);
 
   return data;
-}
+};
---
+++ lib/ignored.ts
@@ -0,0 +1,2 @@
+// This file is just used in by the module-resolve plugin in babel.config.js to ignore node imports.
+// @signalk/client attempts to import 'https' on nodejs, but that raises an error on native.
