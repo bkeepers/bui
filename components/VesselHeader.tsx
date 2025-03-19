3e10febf057b1b06c33fc49cd9c6051bf8132745
Brandon Keepers <brandon@opensoul.org>
2025-03-19 08:39:30 -0400

    Clean up vessel header

--- components/VesselHeader.tsx
+++ components/VesselHeader.tsx
@@ -7,19 +7,11 @@ export function VesselHeader() {
   const data = useSignalK();
 
   return (
-    <View>
-      <View className='flex-row'>
-        <Text className="text-lg font-semibold m-0 p-0 flex-1 leading-none">
-          {data?.name}
-        </Text>
-        <Badge className="text-4xl" variant="muted"><Text>{ data?.navigation?.state?.value }</Text></Badge>
-      </View>
-
-      { data?.mmsi ? (
-        <Text className='text-sm text-muted-foreground m-0 p-0'>
-          MMSI: {data?.mmsi}
-        </Text>
-      ) : null }
+    <View className='flex-row items-center gap-4'>
+      <Text className="text-xl font-semibold leading-none">
+        {data?.name}
+      </Text>
+      <Badge className="text-4xl" variant="muted"><Text>{data?.navigation?.state?.value}</Text></Badge>
     </View>
   );
 }
